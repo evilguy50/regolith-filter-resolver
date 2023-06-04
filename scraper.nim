498f4ab28021c5323c862ef2e8c2a61e7c8a1ec3
evilguy50 <10925997+evilguy50@users.noreply.github.com>
2023-06-04 15:32:13 -0700

    Add version list to filters

--- scraper.nim
+++ scraper.nim
@@ -11,11 +11,13 @@ type
         name: string
         author: string
         url: string
+        versions: seq[string]
 
-proc newFilter(name, author, url: string): Filter =
+proc newFilter(name, author, url: string, versions: seq[string]): Filter =
     result.name = name
     result.author = author
     result.url = url
+    result.versions = versions
 
 proc findRepo(author, repoName: string): string =
     for repo in repoList["known_repos"].to(seq[string]):
@@ -28,35 +30,52 @@ proc cloneRepos() =
     setCurrentDir("Repos")
     for repo in repoList["known_repos"].to(seq[string]):
         let cloneSplit = repo.split("/")
-        var cloneName = ""
-        if cloneSplit[0].startsWith("github.com"):
-            cloneName = cloneSplit[1] & "/" & cloneSplit[2]
+        let cloneName = cloneSplit[1] & "/" & cloneSplit[2]
         let cloneUrl = "http://" & repo
         if not dirExists(cloneName):
             discard execShellCmd(fmt"git clone {cloneUrl} {cloneName}")
     setCurrentDir(getAppDir())
 
 iterator getFilters(): Filter =
+    let root = getCurrentDir()
     for owner in walkDir("Repos"):
         let user = splitPath(owner.path)[1]
         for repo in walkDir(owner.path):
             let repoName = splitPath(repo.path)[1]
-            for filter in walkDir(repo.path):
+            setCurrentDir(root & "/" & repo.path)
+            discard execShellCmd("git tag > tags.txt")
+            for filter in walkDir(getCurrentDir()):
                 let filterName = splitPath(filter.path)[1]
                 if (fileExists(filter.path & "/filter.json")):
-                    yield newFilter(filterName, user, findRepo(user, repoName))
+                    var versions: seq[string] = @["latest"]
+                    let tagList = readFile("tags.txt").split("\n")
+                    for tag in tagList:
+                        if tag.contains(filterName):
+                            let strVersion = tag.replace(filterName & "-", "")
+                            versions.add(strVersion)
+                    yield newFilter(filterName, user, findRepo(user, repoName), versions)
+            setCurrentDir(root)
 
-proc addFilter(f: Filter) =
+proc addFilter(f: Filter): Filter =
+    result = f
     if not resolverJson["filters"].hasKey(f.name):
-        resolverJson["filters"][f.name] = %*{"url": f.url}
+        resolverJson["filters"][f.name] = %*{"url": f.url, "versions": f.versions}
         echo fmt"Added filter {f.name}"
     elif not resolverJson["filters"][f.name]["url"].to(string).contains(f.author):
         let newFilterName = f.author & "_" & f.name
-        resolverJson["filters"][newFilterName] = %*{"url": f.url}
-        echo fmt"Added filter {newFilterName}"
+        if not resolverJson["filters"].hasKey(newFilterName):
+            resolverJson["filters"][newFilterName] = %*{"url": f.url, "versions": f.versions}
+            echo fmt"Added filter {newFilterName}"
+        result.name = newFilterName
 
 cloneRepos()
+var allFilters: seq[Filter] = @[]
 for validFilter in getFilters():
-    validFilter.addFilter()
+    let addedFilter = validFilter.addFilter()
+    allFilters.add(addedFilter)
+
+for existFilter in allFilters:
+    resolverJson["filters"][existFilter.name]["versions"] = %*existFilter.versions
+
 resolverJsonPath.writeFile(resolverJson.pretty())
 sleep(1500)
